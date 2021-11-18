edf7464e34f6d46fe2c9f866505b01dbc192de91
Nymisha sankar <nymishasankar62@gmail.com>
2021-11-18 13:00:36 +0530

    Initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import DataFetching from './components/Useeffecthook/DataFetching';
+import ComponentA from './components/Contexthook/ComponentA';
+import React from 'react';
+
+
+export const UserContext = React.createContext()
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UserContext.Provider value = {'Nymisha'} >
+        <ComponentA/>
+     </UserContext.Provider>
     </div>
   );
 }
