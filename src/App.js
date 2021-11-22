74ecce837bac6a59ad3f2bfed6fa1d9a5da02a5a
Nymisha sankar <nymishasankar62@gmail.com>
2021-11-22 12:07:56 +0530

    count

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import CounterOne from './components/reducerhook/CounterOne';
+import CounterTwo from './components/reducerhook/CounterTwo';
+import CounterThree from './components/reducerhook/CounterThree';
 
 
 
 function App() {
   return (
     <div className="App">
-     <CounterOne/>
+      <CounterThree />
     </div>
   );
 }
